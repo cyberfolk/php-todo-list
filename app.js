d729b01668da71b91f3692bfed05fad798928db9
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-08 18:13:14 +0200

    Add method composeOptions and refact

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ createApp({
   data() {
     return {
       tasks: null,
-      api_url: 'app/Http/Controllers/TasksController/index.php',
+      api_url: 'app/Http/Controllers/TasksController/',
       new_task: '',
       completedTask: [],
     }
@@ -11,59 +11,49 @@ createApp({
   methods: {
 
     add_task() {
-      const options = {
-        url: 'app/Http/Controllers/TasksController/store.php',
-        method: 'POST',
-        headers: { 'Content-Type': 'multipart/form-data' },
-        data: {
-          new_task:
-          {
-            "name": this.new_task,
-            "done": ""
-          }
+      const data = {
+        new_task:
+        {
+          "name": this.new_task,
+          "done": ""
         }
       }
-
+      const options = this.composeOptions('store.php', 'POST', data)
       axios(options)
-        .then(response => {
-          console.log(response);
-          this.tasks = response.data
-        })
+        .then(response => { this.tasks = response.data })
         .catch(error => { console.error(error.message); })
 
     },
+
     completeTask(i) {
       this.completedTask.push(this.tasks[i])
       this.tasks.splice(i, 1);
     },
+
     toggleDoneTask(i) {
-      if (this.tasks[i].done === false) {
-        this.tasks[i].done = true
-      } else {
-        this.tasks[i].done = false
-      }
-      const options = {
-        url: 'app/Http/Controllers/TasksController/toggle.php',
-        method: 'POST',
-        headers: { 'Content-Type': 'multipart/form-data' },
-        data: { index: i }
-      }
+      this.tasks[i].done = this.tasks[i].done ? false : true; //toggle himself
+      const data = { index: i }
+      const options = this.composeOptions('toggle.php', 'POST', data)
 
       axios(options)
         .catch(error => { console.error(error.message); })
+    },
+
+    composeOptions(file, method, data) {
+      return options = {
+        url: this.api_url + file,
+        method: method,
+        headers: { 'Content-Type': 'multipart/form-data' },
+        data: data
+      }
     }
   },
-  mounted() {
 
-    axios
-      .get(this.api_url)
-      .then(response => {
-        console.log(response);
-        this.tasks = response.data
-      })
-      .catch(error => {
-        console.error(error.message);
-      })
+  mounted() {
+    const options = this.composeOptions('index.php', 'GET', '')
+    axios(options)
+      .then(response => { this.tasks = response.data })
+      .catch(error => { console.error(error.message); })
 
 
   }
