3bcf4f9b77083d3c5b952955cf9ed188d8c49fb7
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-08 17:30:00 +0200

    Add col for completed tasks

--- app.js
+++ app.js
@@ -4,35 +4,48 @@ createApp({
     return {
       tasks: null,
       api_url: 'app/Http/Controllers/TasksController/index.php',
-      new_task: ''
+      new_task: '',
+      completedTask: [],
     }
   },
   methods: {
 
     add_task() {
-      console.log('add a new task to the list');
 
-      const data = {
-        new_task: {
-          "name": this.new_task,
-          "completed": ""
+      // axios POST request
+      const options = {
+        url: 'app/Http/Controllers/TasksController/store.php',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        data: {
+          new_task: {
+            "name": this.new_task,
+            "completed": ""
+          }
         }
       }
 
-      axios.post(
-        'app/Http/Controllers/TasksController/store.php',
-        data,
-        {
-          headers: { 'Content-Type': 'multipart/form-data' }
-        }).then(response => {
+      axios(options)
+        .then(response => {
           console.log(response);
           this.tasks = response.data
         })
-        .catch(error => {
-          console.error(error.message);
-        })
+        .catch(error => { console.error(error.message); })
 
     },
+    completeTask(i) {
+      this.completedTask.push(this.tasks[i])
+      this.tasks.splice(i, 1);
+    },
+    toggleDoneTask(i) {
+      if (this.tasks[i].done === false) {
+        this.tasks[i].done = true
+      } else {
+        this.tasks[i].done = false
+      }
+    }
   },
   mounted() {
 
--- index.php
+++ index.php
@@ -9,28 +9,48 @@
     <?php include __DIR__ . "/assets/header.php" ?>
     <main id="app_main">
       <div class="container">
+        <div class="row">
+          <div class="col-3">
+            <h2 class="text-light text-center">Tasks</h2>
+            <div class="card shadow mb-4">
+              <!-- /.add_task -->
+              <ul class="list-group">
+                <li class="list-group-item d-flex justify-content-between" v-for="(task, index) in tasks" :class="{completed : task.done}">
+                  <div @click="toggleDoneTask(index)" role="button">{{task.name}}</div>
+                  <button @click="completeTask(index)" class="bg-danger border-0 rounded py-1 px-2">
+                    <i class="fa-solid fa-trash-can text-light"></i>
+                  </button>
+                </li>
+              </ul>
+            </div>
+            <!-- /.card -->
 
-        <div class="card shadow col-3 m-auto mb-4">
-          <!-- /.add_task -->
-          <ul class="list-group">
-            <li class="list-group-item d-flex justify-content-between" v-for="(task, index) in tasks" :class="{completed : task.done}">
-              <div @click="toggleDoneTask(index)" role="button">{{task.name}}</div>
-              <button @click="completeTask(index)" class="bg-danger border-0 rounded py-1 px-2">
-                <i class="fa-solid fa-trash-can text-light"></i>
-              </button>
-            </li>
-          </ul>
-        </div>
-        <!-- /.card -->
-
-        <form class="add_task col-3 m-auto">
-          <div class="input-group mb-3">
-            <input type="text" name="new_task" id="new_task" class="form-control " v-model="new_task" @keyup.enter="add_task" placeholder="Type a task here">
-            <button class="input-group-text" @click="add_task">Add</button>
+            <form class="add_task">
+              <div class="input-group mb-3">
+                <input type="text" name="new_task" id="new_task" class="form-control " v-model="new_task" @keyup.enter="add_task" placeholder="Type a task here">
+                <button class="input-group-text" @click="add_task">Add</button>
+              </div>
+            </form>
+            <!-- /.add_task -->
           </div>
-        </form>
-        <!-- /.add_task -->
+          <!-- /.col-3 -->
 
+
+          <div class="col-3">
+            <div class="completedTask mt-3 mt-lg-0">
+              <h2 class="text-light text-center">Completed Tasks</h2>
+              <ul v-if="completedTask.length > 0" class="list-group">
+                <li v-for="(task, index) in completedTask" class="list-group-item">{{task.text}}</li>
+              </ul>
+              <div class="alert alert-danger " role="alert" v-else>
+                <strong>Al momento non hai completato nessuna Task</strong>
+              </div>
+            </div>
+            <!-- /.completedTask -->
+          </div>
+          <!-- /.col-3 -->
+        </div>
+        <!-- /.row -->
       </div>
       <!-- /.container -->
     </main>
