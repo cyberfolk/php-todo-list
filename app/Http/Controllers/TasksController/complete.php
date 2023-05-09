5d87a14abb7da12d39f1ad1d6e8c39f5cba242f8
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-09 14:50:12 +0200

    Add logic to complete and move task in done.json

--- app/Http/Controllers/TasksController/complete.php
+++ app/Http/Controllers/TasksController/complete.php
@@ -1,30 +1,32 @@
 <?php
 $todo_path = 'tasksTodo.json';
-$deleted_path = 'tasksDeleted.json';
+$done_path = 'tasksDone.json';
 
 if (isset($_POST['index'])) {
     $index = $_POST['index'];
 
     // read the json file with file_get_contents
     $todo_string = file_get_contents($todo_path);
-    $deleted_string = file_get_contents($deleted_path);
+    $done_string = file_get_contents($done_path);
     // convert the json_string into an associative array with json_decode()
     $todo_array = json_decode($todo_string, true);
-    $deleted_array = json_decode($deleted_string, true);
+    $done_array = json_decode($done_string, true);
+
+    $todo_array[$index]["done"] = !$todo_array[$index]["done"];
 
     // Get the deleted from todo_array
-    $deleted_task = $todo_array[$index];
+    $done_task = $todo_array[$index];
     // Add the deleted task to the array
-    array_unshift($deleted_array, $deleted_task);
+    array_unshift($done_array, $done_task);
     // remove object in array by index
     array_splice($todo_array, $index, 1); //array_splice accepts 3 parameters (array, start, length) removes the given array and then normalizes the index
 
     // convert the array back into a json string
     $todo_json = json_encode($todo_array);
-    $deleted_json = json_encode($deleted_array);
+    $done_json = json_encode($done_array);
     // replace the file content using file_put_contents()
     file_put_contents($todo_path, $todo_json);
-    file_put_contents($deleted_path, $deleted_json);
+    file_put_contents($done_path, $done_json);
 
     // add header application/json
     header('Content-Type: application/json');
