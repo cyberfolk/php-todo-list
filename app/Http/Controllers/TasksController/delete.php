1bc6fb67646255c7d3422cd744eb34c0a707b2e3
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-09 14:04:46 +0200

    Add ro tasksDeleted.json when delete task

--- app/Http/Controllers/TasksController/delete.php
+++ app/Http/Controllers/TasksController/delete.php
@@ -1,28 +1,33 @@
 <?php
-$file_path = 'tasks.json';
+$todo_path = 'tasksTodo.json';
+$deleted_path = 'tasksDeleted.json';
 
 if (isset($_POST['index'])) {
     $index = $_POST['index'];
 
     // read the json file with file_get_contents
-    $tasks_string = file_get_contents($file_path);
+    $todo_string = file_get_contents($todo_path);
+    $deleted_string = file_get_contents($deleted_path);
     // convert the json_string into an associative array with json_decode()
-    $tasks_array = json_decode($tasks_string, true);
+    $todo_array = json_decode($todo_string, true);
+    $deleted_array = json_decode($deleted_string, true);
 
+    // Get the deleted from todo_array
+    $deleted_task = $todo_array[$index];
+    // Add the deleted task to the array
+    array_unshift($deleted_array, $deleted_task);
     // remove object in array by index
-    array_splice($tasks_array, $index, 1); //array_splice accepts 3 parameters (array, start, length) removes the given array and then normalizes the index
-    // OR 
-    // unset($tasks_array[$index]); //removes the array at given index
-    // $reindex = array_values($tasks_array); //normalize index
-    // $tasks_array = $reindex; //update variable 
+    array_splice($todo_array, $index, 1); //array_splice accepts 3 parameters (array, start, length) removes the given array and then normalizes the index
 
     // convert the array back into a json string
-    $tasks_array = json_encode($tasks_array);
+    $todo_array = json_encode($todo_array);
+    $deleted_array = json_encode($deleted_array);
     // replace the file content using file_put_contents()
-    file_put_contents($file_path, $tasks_array);
+    file_put_contents($todo_path, $todo_array);
+    file_put_contents($deleted_path, $deleted_array);
+
     // add header application/json
     header('Content-Type: application/json');
-
     header("Access-Control-Allow-Origin: http://localhost:5173");
     header("Access-Control-Allow-Headers: X-Requested-With");
 }
