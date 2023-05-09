c9129e35562ba6cc2ac5931260e3198443498d55
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-09 23:27:14 +0200

    Unify Logic to delete task

--- app/Http/Controllers/TasksController/delete.php
+++ app/Http/Controllers/TasksController/delete.php
@@ -1,29 +1,29 @@
 <?php
 include __DIR__ . '/helper/functions.php';
-$todo_path = 'tasksTodo.json';
 $deleted_path = 'tasksDeleted.json';
+$start_path = $_POST['start_path']; // The directory that contains the task to move - tasksTodo.json or tasksDone.json
 
 if (isset($_POST['index'])) {
     $index = $_POST['index'];
 
     // read the json file with file_get_contents
-    $todo_json = file_get_contents($todo_path);
+    $start_json = file_get_contents($start_path);
     $deleted_json = file_get_contents($deleted_path);
     // convert the json_json into an associative array with json_decode()
-    $todo_array = json_decode($todo_json, true);
+    $start_array = json_decode($start_json, true);
     $deleted_array = json_decode($deleted_json, true);
 
-    // Get the deleted from todo_array
-    $deleted_task = $todo_array[$index];
+    // Get the deleted from start_array
+    $deleted_task = $start_array[$index];
     // Add the deleted task to the array
     array_unshift($deleted_array, $deleted_task);
     // remove object in array by index
-    array_splice($todo_array, $index, 1); //array_splice accepts 3 parameters (array, start, length) removes the given array and then normalizes the index
+    array_splice($start_array, $index, 1); //array_splice accepts 3 parameters (array, start, length) removes the given array and then normalizes the index
 
     // convert the array back into a json string
-    $todo_json = json_encode($todo_array);
+    $start_json = json_encode($start_array);
     $deleted_json = json_encode($deleted_array);
     // replace the file content using file_put_contents()
-    file_put_contents($todo_path, $todo_json);
+    file_put_contents($start_path, $start_json);
     file_put_contents($deleted_path, $deleted_json);
 }
