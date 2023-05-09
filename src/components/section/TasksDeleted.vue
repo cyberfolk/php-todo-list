d94fe6d2af492ef21aa077e1bf692eac15044a4c
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-09 17:35:11 +0200

    Implement restoreAll and recycleBin

--- src/components/section/TasksDeleted.vue
+++ src/components/section/TasksDeleted.vue
@@ -13,6 +13,18 @@ export default {
             state.fetchTodo()
             state.fetchDeleted()
         },
+        restoreAll() {
+            const options = state.composeOptions('restoreAll.php', 'POST')
+            axios(options).catch(error => { console.error(error.message); })
+            state.fetchTodo()
+            state.fetchDeleted()
+        },
+        recycleBin() {
+            const options = state.composeOptions('recycleBin.php', 'POST')
+            axios(options).catch(error => { console.error(error.message); })
+            state.fetchTodo()
+            state.fetchDeleted()
+        }
     }
 };
 </script>
@@ -20,7 +32,19 @@ export default {
 <template>
     <!-- Not yet implemented -->
     <section id="tasks_deleted" class="col-4">
-        <h2 class="text-light text-center pb-1">Deleted Tasks</h2>
+        <div class="d-flex align-items-end mb-2 justify-content-center">
+            <h2 class="text-light text-center m-0 pe-2">Deleted Tasks</h2>
+            <div>
+                <button @click="recycleBin(index)" class="bg-danger border-0 rounded py-1 px-2 me-1" title="Recycle Bin">
+                    <i class="fa-solid fa-recycle text-light"></i>
+                </button>
+            </div>
+            <div>
+                <button @click="restoreAll()" class="bg-warning border-0 rounded py-1 px-2 me-1" title="Restore All">
+                    <i class="fa-solid fa-trash-can-arrow-up text-light"></i>
+                </button>
+            </div>
+        </div>
         <ul v-if="state.tasksDeleted.length" class="list-group">
             <li v-for="(task, index) in state.tasksDeleted" class="list-group-item d-flex justify-content-start align-items-center fw-bold ps-2">
                 <button @click="restoreTask(index)" class="bg-warning border-0 rounded py-1 px-2 me-1">
@@ -32,6 +56,7 @@ export default {
         <div class="alert alert-danger " role="alert" v-else>
             <strong>Al momento non hai completato nessuna Task</strong>
         </div>
+
         <div class="text-end"><small class="text-light">NB. Questi valori non sono persistenti</small></div>
         <div class="text-end"><small class="text-light">NB. Funzionalità non ancora implementata</small></div>
     </section>
